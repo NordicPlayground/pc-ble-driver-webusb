86f1547c27e250664072e35787d0a14bff32911f
Mathias Aarseth Pedersen <Mathias.Pedersen@nordicsemi.no>
2018-06-12 16:49:51 +0200

    Added support for adding gatts characteristics

---
+++ examples/gapConnect/characteristic.js
@@ -0,0 +1,84 @@
+class bleGattsCharHandles {
+    constructor () {
+      this.data = Module._malloc(2*4);
+      this.dw = new DataView(Module.HEAPU8.buffer, this.data, this.data + 8);
+    }
+
+    getValueHandle() {
+        return this.dw.getUint16(0);
+    }
+    userDescHandle() {
+        return this.dw.getUint16(1);
+    }
+    cccdHandle() {
+        return this.dw.getUint16(2);
+    }
+    sccdHandle() {
+        return this.dw.getUint16(3);
+    }
+    clean () {
+        Module._free(this.data);
+    }
+}
+
+
+class Characteristic {
+    constructor() {
+        this.maxDatalen = 32;
+        let initData = new Uint8Array(this.maxDatalen);
+        initData[0]=1;
+        initData[1]=2;
+        this.initDataPtr = Module._malloc(this.maxDatalen);
+        Module.HEAPU8.set(initData, this.initDataPtr);
+
+        this.charHandle = new bleGattsCharHandles();
+        this.cccd_md = Module.ccall('createGattsAttrMd', 'number', ['number', 'number', 'number', 'number'], [0,1,0,0]);
+        this.attr_md = Module.ccall('createGattsAttrMd', 'number', ['number', 'number', 'number', 'number'], [1,1,0,0]);
+
+        this.char_md = Module.ccall('createCharMd', 'number', ['number'], [this.cccd_md]);
+        this.uuid = new bleUUIDt();
+
+        this.uuid.setUUID(0x372A);
+        this.uuid.setType(1);
+        this.attr_char_value = Module.ccall('createAttCharValue', 'number', ['number', 'number', 'number', 'number', 'number', 'number'], [this.uuid.data, this.attr_md, 2/*initData.length*/, 0, this.maxDatalen, this.initDataPtr]);
+
+    }
+    clean () {
+        Module._free(this.initDataPtr);
+        Module._free(this.cccd_md);
+        Module._free(this.attr_md);
+        Module._free(this.char_md);
+        Module._free(this.attr_char_value);
+        this.charHandle.clean();
+        this.uuid.clean();
+    }
+}
+
+class bleUUIDt {
+    constructor() {
+        this.data = Module._malloc(4);
+        this.dw = new DataView(Module.HEAPU8.buffer, this.data, this.data + 4);
+    }
+
+    getUUID() {
+        return this.dw.getUint16(0);
+    }
+    setUUID(value) {
+        this.dw.setUint16(0, value);
+    }
+    getType() {
+        return this.dw.getUint8(2); // third byte
+    }
+    setType(value) {
+        this.dw.setUint8(2, value)
+    }
+    clean () {
+        Module._free(this.data);
+    }
+}
+
+class bleUUID128t {
+    constructor() {
+
+    }
+}
