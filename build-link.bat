3e7f472eaa332b435226b93cc64b4b51c5a223b6
Mathias Aarseth Pedersen <Mathias.Pedersen@nordicsemi.no>
2018-07-17 13:25:08 +0200

    Changed build optimization level to from O3 to O2

--- build-link.bat
+++ build-link.bat
@@ -1,5 +1,5 @@
 emcmake cmake && ^
 emmake make && ^
-em++ -O3 --js-opts 1 -s WASM=1 -o build/v3/pc_ble_driver_sd_api_v3.js libpc_ble_driver_static_sd_api_v3.a  ^
+em++ -O2 --js-opts 1 -s WASM=1 -o build/v3/pc_ble_driver_sd_api_v3.js libpc_ble_driver_static_sd_api_v3.a  ^
 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'getValue', 'setValue', 'cwrap', 'writeArrayToMemory']" ^
 && del libpc_ble_driver_static_sd_api_v3.a
