bad94d806b368de2732fa1f73c2c7ee7f05ee85f
Mathias Aarseth Pedersen <Mathias.Pedersen@nordicsemi.no>
2018-07-17 12:44:30 +0200

    Updated serial to use interfaces as specified in connectivity usbd

--- src/js/transport/serial.js
+++ src/js/transport/serial.js
@@ -7,7 +7,7 @@ const serial = {};
 
     serial.requestPort = () => {
         const filters = [
-          { vendorId: 0x1366, productId: 0x1015 },
+          /*{ vendorId: 0x1366, productId: 0x1015 },*/
         ];
         return navigator.usb.requestDevice({ filters }).then(
           device => new serial.Port(device),
@@ -20,14 +20,14 @@ const serial = {};
 
     serial.Port.prototype.connect = function(baudRate) {
         const readLoop = () => {
-            this.device_.transferIn(3, 512).then(result => {
+            this.device_.transferIn(1, 64).then(result => {
                 this.onReceive(result.data);
                 readLoop();
             }, error => {
                 this.onReceiveError(error);
             });
         };
-
+        let buffer = new Uint8Array(7);
         // Bytes 0-3 are baudrate LE. Byte 4 is stop bits. Byte 5 is parity (0 = no parity),Byte 6 is number of data bits.
         let controlData = new Uint8Array([baudRate & 0xFF, (baudRate>>8) & 0xFF, (baudRate>>16) & 0xFF, (baudRate>>24) & 0xFF, 0, 0, 8]);
         return this.device_.open()
@@ -39,22 +39,34 @@ const serial = {};
             })
             .then(() => this.device_.claimInterface(0))
             .then(() => this.device_.claimInterface(1))
+            .then(() => this.device_.claimInterface(2))
             .then(() => this.device_.selectAlternateInterface(1, 0))
+            .then(() => this.device_.selectAlternateInterface(2, 0))
+
             .then(() => this.device_.controlTransferOut({
                 requestType: 'class',
                 recipient: 'interface',
                 request: 0x20,
                 value: 0x00,
-                index: 0x00 }, controlData))
+                index: 0x01 }, controlData))
 
             .then(() => this.device_.controlTransferOut({
                 requestType: 'class',
                 recipient: 'interface',
                 request: 0x22,
+                value: 0x03,
+                index: 0x01 }))
+            .then(() => this.device_.controlTransferIn({
+                requestType: 'class',
+                recipient: 'interface',
+                request: 0x21,
                 value: 0x00,
-                index: 0x00 }))
+                index: 0x01 }, 7))
             .then(() => {
                 readLoop();
+            })
+            .catch(error => {
+                console.log(error)
             });
     };
 
@@ -63,6 +75,6 @@ const serial = {};
     };
 
     serial.Port.prototype.send = function(data) {
-        return this.device_.transferOut(4, data);
+        return this.device_.transferOut(1, data);
     };
 })();
