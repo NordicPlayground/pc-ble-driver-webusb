bc22c0ac3c80e390e50ee8de5ab61f54562bd31e
Mathias Aarseth Pedersen <Mathias.Pedersen@nordicsemi.no>
2018-07-18 11:57:55 +0200

    Removed unused build scripts for windows

--- build-link.bat
+++
@@ -1,5 +0,0 @@
-emcmake cmake && ^
-emmake make && ^
-em++ -O2 --js-opts 1 -s WASM=1 -o build/v3/pc_ble_driver_sd_api_v3.js libpc_ble_driver_static_sd_api_v3.a  ^
--s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'getValue', 'setValue', 'cwrap', 'writeArrayToMemory']" ^
-&& del libpc_ble_driver_static_sd_api_v3.a
